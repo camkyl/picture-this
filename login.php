a2c99b177ab4c2247cc2d75a00f4053903f51289
Camilla <cks@live.se>
2019-12-16 10:39:21 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+// Front-end
+declare(strict_types=1);
+
+require __DIR__ . '/views/header.php';
+
+?>
+
+<section class="sign-in-page">
+    <h1>Sign in</h1>
+    <form action="/app/users/login.php" method="post" class="sign-in-form">
+        <div>
+            <!--Error message-->
+            <?php
+            if (isset($_SESSION['email-not-found'])) {
+                echo $_SESSION['email-not-found'];
+            }
+            unset($_SESSION['email-not-found']);
+
+            if (isset($_SESSION['error'])) {
+                echo $_SESSION['error'];
+            }
+            unset($_SESSION['error']);
+            ?>
+        </div>
+
+        <div class="sign-in-form__email">
+            <label for="email">Email</label>
+            <input type="email" name="email" placeholder="Email" class="not-logged-in-input" required>
+        </div>
+
+        <div class="sign-in-form__password">
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Password" class="not-logged-in-input" required>
+        </div>
+
+        <button type="submit" class="sign-in-form__button">Sign in</button>
+    </form>
+    <div class="sign-in-page__register">
+        <p>Don't have an account?</p>
+        <a href="register.php">Sign up!</a>
+    </div>
+</section>
