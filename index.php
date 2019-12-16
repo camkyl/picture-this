4c777dbf16ff5a5766c0b26eb51e5f4d8eccefef
Camilla <cks@live.se>
2019-12-16 10:39:17 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+require(__DIR__ . '/views/header.php');
+?>
+
+<div>
+    <p>This is the index-file</p>
+</div>
+
+<?php require(__DIR__ . '/views/footer.php'); ?>
