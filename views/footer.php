def7924073ff1d765c708a6b8a9198dbeb9acb6f
Camilla <cks@live.se>
2019-12-21 23:41:01 +0100

    some css

--- views/footer.php
+++ views/footer.php
@@ -6,7 +6,15 @@ declare(strict_types=1);
 ?>
 
 <footer>
-    <a href="/create-post.php">Create post</a>
+    <a href="/index.php">
+        <img src="/views/icons/home2.svg" alt="Home" class="icon">
+    </a>
+    <a href="/create-post.php">
+        <img src="/views/icons/add.svg" alt="Add post" class="icon">
+    </a>
+    <a href="#">
+        <img src="/views/icons/heart.svg" alt="Liked by me" class="icon">
+    </a>
 </footer>
 
 </body>
