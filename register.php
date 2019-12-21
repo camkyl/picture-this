9c0db3f5d581e8b3c3b09e31256a09ebdd2ade90
Camilla <cks@live.se>
2019-12-21 22:22:51 +0100

    added css for log in and sign up page

--- register.php
+++ register.php
@@ -7,40 +7,49 @@ require __DIR__ . '/views/header.php';
 
 ?>
 
-<section class="sign-up-page">
-    <h1>Sign up</h1>
-    <form action="/app/users/register.php" method="post" class="sign-up-form">
-        <div class="error-message">
-            <?php
-            displayErrorMessage();
-            ?>
+<section class="sign-up-page flex-col-cen">
+
+    <img src="/views/icons/picture-this-white.svg" alt="Picture This, sign up page">
+
+    <div class="form-frame flex-col-cen">
+
+        <h1>Sign up</h1>
+
+        <form action="/app/users/register.php" method="post" class="sign-up-form">
+            <div class="error-message">
+                <?php
+                displayErrorMessage();
+                ?>
+            </div>
+
+            <div class="sign-up-form__group flex-col">
+                <label for="first-name">First name</label>
+                <input type="text" name="first-name" placeholder="Type your first name" class="not-logged-in-input" required>
+            </div>
+
+            <div class="sign-up-form__group flex-col">
+                <label for="last-name">Last name</label>
+                <input type="text" name="last-name" placeholder="Type your last name" class="not-logged-in-input" required>
+            </div>
+
+            <div class="sign-up-form__group flex-col">
+                <label for="email">Email</label>
+                <input type="email" name="email" placeholder="Type your email" class="not-logged-in-input" required>
+            </div>
+
+            <div class="sign-up-form__group flex-col">
+                <label for="password">Password</label>
+                <input type="password" name="password" placeholder="Type your password" class="not-logged-in-input" required>
+            </div>
+
+            <div class="flex-col-cen">
+                <button type="submit" class="sign-up-form__button">Sign up</button>
+            </div>
+        </form>
+
+        <div class="sign-up-page__login flex-col-cen c-white">
+            <p>Have an account?</p>
+            <a href="login.php">Log in!</a>
         </div>
-
-        <div class="sign-up-form__first-name">
-            <label for="first-name">First name</label>
-            <input type="text" name="first-name" placeholder="First name..." class="not-logged-in-input" required>
-        </div>
-
-        <div class="sign-up-form__last-name">
-            <label for="last-name">Last name</label>
-            <input type="text" name="last-name" placeholder="Last name..." class="not-logged-in-input" required>
-        </div>
-
-        <div class="sign-up-form__email">
-            <label for="email">Email</label>
-            <input type="email" name="email" placeholder="name@email.com" class="not-logged-in-input" required>
-        </div>
-
-        <div class="sign-up-form__password">
-            <label for="password">Password</label>
-            <input type="password" name="password" placeholder="Password" class="not-logged-in-input" required>
-        </div>
-
-        <button type="submit" class="sign-in-form__button">Sign up</button>
-    </form>
-
-    <div class="sign-up-page__login">
-        <p>Have an account?</p>
-        <a href="login.php">Sign in!</a>
-    </div>
+    </div> <!-- /form-frame -->
 </section>
