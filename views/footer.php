50e0f7445399864a394792609b339415ba8bb208
Camilla <cks@live.se>
2020-01-06 13:27:12 +0100

    changed layout on add post-icon

--- views/footer.php
+++ views/footer.php
@@ -9,9 +9,13 @@ declare(strict_types=1);
     <a href="/index.php">
         <img src="/views/icons/home2.svg" alt="Home" class="icon">
     </a>
-    <a href="/create-post.php">
-        <img src="/views/icons/add.svg" alt="Add post" class="icon">
-    </a>
+
+    <div class="add__background">
+        <a href="/create-post.php">
+            <img src="/views/icons/add-white2.svg" alt="Add post" class="add">
+        </a>
+    </div>
+
     <a href="#">
         <img src="/views/icons/heart.svg" alt="Liked by me" class="icon">
     </a>
