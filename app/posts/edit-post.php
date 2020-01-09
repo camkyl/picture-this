c2cd19145e02ba84fce1a8a9d3a015a373e6a487
Camilla <cks@live.se>
2020-01-09 19:49:07 +0100

    edit post

--- app/posts/edit-post.php
+++ app/posts/edit-post.php
@@ -6,3 +6,21 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 // In this file we edit users posts in the database
+
+if(isset($_POST['edit-post-caption'])){
+    $editedCaption = filter_var($_POST['edit-post-caption'], FILTER_SANITIZE_STRING);
+    $postId = $_GET['id'];
+    
+    $statement = $pdo->prepare('UPDATE posts SET post_caption = :editedCaption WHERE id = :postId');
+
+    if(!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $statement->execute([
+        ':editedCaption' => $editedCaption,
+        ':postId' => $postId
+    ]);
+
+    redirect('/');
+}
--- edit-post.php
+++ edit-post.php
@@ -3,23 +3,31 @@
 declare(strict_types=1);
 
 require __DIR__ . '/views/header.php';
+
 require __DIR__ . '/views/navigation.php';
 
 isLoggenIn();
+
+$postId = $_GET['id'];
+
+$postToEdit = getPostByID((int) $postId, $pdo);
+
 ?>
 
-<section class="edit-post">
+<section class="edit-post flex-col-cen">
     <h2 class="mt15">Edit post</h2>
-    <form action="/app/posts/create-post.php" method="post" enctype="multipart/form-data" class="flex-col-cen">
-        <div class="">
-            <img src="<?php ?>">
+    <form action="/app/posts/edit-post.php?id=<?php echo $postToEdit['id']; ?>" method="post" enctype="multipart/form-data" class="flex-col-cen">
+        <div class="create-post__image-frame mt15">
+            <img src="/app/posts/uploads/<?php echo $postToEdit['post_image']; ?>" alt="Image to post being edited">
         </div>
 
-        <div class="create-post__caption flex-col mt15">
-            <label for="post-caption">Caption:</label>
-            <textarea name="post-caption" class="mt15">hej</textarea>
+        <div class="create-post__caption flex-col-cen mt15">
+            <div class="align-left">
+                <label for="edit-post-caption">Edit caption:</label>
+            </div>
+            <textarea name="edit-post-caption" class="mt15"><?php echo $postToEdit['post_caption']; ?></textarea>
         </div>
 
-        <button type="submit" class="mt15">Share</button>
+        <button type="submit" class="mt15">Update</button>
     </form>
 </section>
